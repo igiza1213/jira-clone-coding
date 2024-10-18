0130cb286d706a6fc3b0f4100fe82f033c768160
igiza1213 <igiza1213@gmail.com>
2024-10-18 12:25:26 +0900

    feature : settring up app wirte (wip)

--- jira-first/src/app/page.tsx
+++ jira-first/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select } from "@/components/ui/select";
+import { createAdminClient } from "@/lib/appwrite";
 
 export default function Home() {
   return (
