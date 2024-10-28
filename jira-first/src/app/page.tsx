b3af882220d8b12d768c7fb0feb252f7c401c4e1
igiza1213 <igiza1213@gmail.com>
2024-10-28 23:11:11 +0900

    feature : build session middleware

--- jira-first/src/app/page.tsx
+++ jira-first/src/app/page.tsx
@@ -1,20 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Select } from "@/components/ui/select";
-import { createAdminClient } from "@/lib/appwrite";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
-    <div className="flex gap-4">
-      <Input />
-      <Select> </Select>
-      <Button size={"xs"}>Primary</Button>
-      <Button variant={"secondary"}>Secondary</Button>
-      <Button variant={"destructive"}>Destructive</Button>
-      <Button variant={"ghost"}>Ghost</Button>
-      <Button variant={"muted"}>Muted</Button>
-      <Button variant={"outline"}>Outline</Button>
-      <Button variant={"teritrary"}>teritary</Button>
+    <div className="">
+      Only visible to authorized users.{" "}
+      <Button onClick={() => mutate()}>logout</Button>
     </div>
   );
 }
